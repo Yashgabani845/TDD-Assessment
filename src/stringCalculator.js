d8be54f67937b0679f9b3e8e2079ad35991e20e1
Yashgabani845 <gabaniyash845@gmail.com>
2025-07-03 21:46:36 +0530

    [Refactor 1.1] Added Implementation for numbers as input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,6 +2,9 @@ function Add ( str ){
     if(str === ""){
         return 0;
     }
+    const numbers = str.split(",").map(s => Number(s.trim()));
+    return numbers.reduce((accumulator, currentValue) => accumulator + currentValue , 0 );
+    
 }
 
 module.exports= { Add };
--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -8,7 +8,8 @@ describe("Addition", () => {
      test('should return sum of numbers on numbers', ()=>{
         expect(Add("1,2")).toBe(3);
         expect(Add("1,2,3")).toBe(6);
-        expect(Add("1")).toBe(1);
+        expect(Add("5")).toBe(5);
+        expect(Add("1,")).toBe(1);
      })
 
 });
