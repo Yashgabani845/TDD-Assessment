e4c391a40b72118cb2ba13229d3d2a3a7f45fc97
Yashgabani845 <gabaniyash845@gmail.com>
2025-07-03 22:30:24 +0530

    [Test 3] Added Passed Test for newline as delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,11 +3,12 @@ function Add ( str ){
         return 0;
     }
     const numbers = str
+    .replace(/\n/g, ",")
     .split(",")
     .map(s => s.trim())
     .filter(s => s !== "")
     .map(Number);
-    
+
     return numbers.reduce((accumulator, currentValue) => accumulator + currentValue , 0 );
     
 }
