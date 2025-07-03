0b15a4cac6f32a65c89de7e3b69f45b339beaa9f
Yashgabani845 <gabaniyash845@gmail.com>
2025-07-03 22:41:58 +0530

    [Test 4] Added Passed  Test for Support different delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -2,8 +2,17 @@ function Add ( str ){
     if(str === ""){
         return 0;
     }
+    let delimiters = [",", "\n"];
+    
+    if(str.startsWith("//")){
+        let delimiter = str.substring(2,3);
+        delimiters.push(delimiter);
+        str = str.substring(4);
+    }
+
+    const regx = new RegExp(`[${delimiters.join("")}]`);
     const numbers = str
-    .split(/[\n,]/)
+    .split(regx)
     .map(s => s.trim())
     .filter(s => s !== "")
     .map(Number);
